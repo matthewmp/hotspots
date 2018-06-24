fb052fa74ac8c9c8b9213a0385a59442fb4e1527
Matt Palumbo <matt.m.palumbo@gmail.com>
2018-06-24 09:23:51 -0400

    added multi resizing, reorganized render functions for resizing to the general draw function

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const btnUpload = document.getElementById('inp-file');  // Open File Button
 const btnAdd = document.getElementById('btn-add');  // Add new Block Button
 
 const inpX = document.getElementById('inpX');   // Position x field
-const inpy = document.getElementById('inpy');   // Position y field
+const inpY = document.getElementById('inpY');   // Position y field
 const inpW = document.getElementById('inpW');   // Width field
 const inpH = document.getElementById('inpH');   // Height Field
 
@@ -31,6 +31,9 @@ const btnHdown = document.getElementById('btn-h-down');
 const formResizeCanvas = document.getElementById('form-resizeCanvas');
 const inpResizeWidth = document.getElementById('inp-resizeCanvasWidth');
 const inpResizeHeight = document.getElementById('inp-resizeCanvasHeight');
+
+// Set All Form
+const formSetAll = document.getElementById('set-all');
 
 // Image Variables
 var imgHolder = document.getElementById('img-holder');
@@ -114,157 +117,110 @@ function showBoxData(){
 
 // Set listeners to move block forward on x axis
 btnXup.addEventListener('mousedown', function(e){
-        xUpInterval = setInterval(function(){
-               state.boxArr.forEach(function(box){
-                        if(box.selected){                        
-                                box.x += 1;
-                                if(box.x + box.w >= canvas.width){
-                                        box.x = canvas.width - box.w;
-                                }
-                                showBoxData();
+        e.preventDefault();
+       state.boxArr.forEach(function(box){
+                if(box.selected){                        
+                        box.x += 1;
+                        if(box.x + box.w >= canvas.width){
+                                box.x = canvas.width - box.w;
                         }
-                });
-        }, 20)      
-});
+                        showBoxData();
 
-btnXup.addEventListener('mouseup', function(e){
-                clearInterval(xUpInterval);
+                }
+        });
 });
 
 // Move block backwards
 btnXdown.addEventListener('mousedown', function(e){
-        xDownInterval = setInterval(function(){
-                state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.x -= 1;
-                                if(box.x <= 0){
-                                        box.x = 0;
-                                }
-                                showBoxData();
+        state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.x -= 1;
+                        if(box.x <= 0){
+                                box.x = 0;
                         }
-                })
-                
-        }, 20)      
-});
-
-btnXdown.addEventListener('mouseup', function(e){
-                clearInterval(xDownInterval);
+                        showBoxData();
+                }
+        });
 });
 
 // Move block's y axis upward
 btnYup.addEventListener('mousedown', function(e){
-        yUpInterval = setInterval(function(){
-               state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.y -= 1;
-                                if(box.y  <= 0){
-                                        box.y = 0;
-                                }
-                                showBoxData();
+       state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.y -= 1;
+                        if(box.y  <= 0){
+                                box.y = 0;
                         }
-                });
-        }, 20)      
+                        showBoxData();
+                }
+        });
 });
-
-btnYup.addEventListener('mouseup', function(e){
-                clearInterval(yUpInterval);
-})
 
 //  Move block's y axis downward
 btnYdown.addEventListener('mousedown', function(e){
-        yInterval = setInterval(function(){
-                state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.y += 1;
-                                if(box.y + box.h >= canvas.height){
-                                box.y = canvas.height - box.h;
-                }
-                showBoxData();
-                        }        
-                });
-        }, 20)      
-});
-
-btnYdown.addEventListener('mouseup', function(e){
-                clearInterval(yInterval);
+        state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.y += 1;
+                        if(box.y + box.h >= canvas.height){
+                        box.y = canvas.height - box.h;
+        }
+        showBoxData();
+                }        
+        });
 });
 
 // Increase Block Height
 btnHup.addEventListener('mousedown', function(e){
-        heightIntervalUp = setInterval(function(){
-               state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.h += 1;
-                                if(box.h >= canvas.height){
-                                        box.y = canvas.height;
-                                }
-                                showBoxData();
+       state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.h += 1;
+                        if(box.h >= canvas.height){
+                                box.y = canvas.height;
                         }
-                });
-        }, 20)      
-});
-
-btnHup.addEventListener('mouseup', function(e){
-                clearInterval(heightIntervalUp);
+                        showBoxData();
+                }
+        });
 });
 
 // Decrease Block Height
 btnHdown.addEventListener('mousedown', function(e){
-        heightIntervalDown = setInterval(function(){
-               state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.h -= 1;
-                                if(box.h <= 5){
-                                        box.h = 5;
-                                }
-                                showBoxData();
+       state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.h -= 1;
+                        if(box.h <= 5){
+                                box.h = 5;
                         }
-                });
-        }, 20)      
-});
-
-btnHdown.addEventListener('mouseup', function(e){
-                clearInterval(heightIntervalDown);
+                        showBoxData();
+                }
+        });
 });
 
 // Increase Block Width
 btnWup.addEventListener('mousedown', function(e){
-        widthIntervalDown = setInterval(function(){
-               state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.w += 1;
-                                if(box.w >= canvas.width){
-                                        box.w = canvas.width;
-                                }
-                                showBoxData();
+       state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.w += 1;
+                        if(box.w >= canvas.width){
+                                box.w = canvas.width;
                         }
-                });
-        }, 20)      
-});
-
-btnWup.addEventListener('mouseup', function(e){
-                clearInterval(widthIntervalDown);
+                        showBoxData();
+                }
+        });
 });
 
 // Decrease Block Width
 btnWdown.addEventListener('mousedown', function(e){
-        widthIntervalUp = setInterval(function(){
-               state.boxArr.forEach(function(box){
-                        if(box.selected){
-                                box.w -= 1;
-                                if(box.w <= 5){
-                                        box.w = 5;
-                                }
-                                showBoxData();
+       state.boxArr.forEach(function(box){
+                if(box.selected){
+                        box.w -= 1;
+                        adjustResizeBoxes(box);
+                        if(box.w <= 5){
+                                box.w = 5;
                         }
-                });
-        }, 20)      
+                        showBoxData();
+                }
+        });    
 });
-
-btnWdown.addEventListener('mouseup', function(e){
-                clearInterval(widthIntervalUp);
-});
-
 
 // Toolbar Movements
 let toolbarObj = {
@@ -328,19 +284,35 @@ let mousePosObj = {
 
 let keyBoardObj = {
         multiSelect: false,
+        multiSelectResize: false
+}
+
+let resizeObj = {
+        xStart: 0,
+        yStart: 0,
+        xDiff: 0,
+        yDiff: 0,
+        element: '',
+        getDiff: function(x,y){
+                return {x: x - this.xStart, y: y - this.yStart}
+        }
 }
 
 
-canvas.addEventListener('mousedown', drag);
 window.addEventListener('keypress', function(e){
         if(e.keyCode === 115){
                 keyBoardObj.multiSelect = true;
+        } 
+        else if(e.keyCode === 114){
+                keyBoardObj.multiSelectResize = true;
         }
 });
 
 window.addEventListener('keyup', function(){
         keyBoardObj.multiSelect = false;
-})
+});
+
+canvas.addEventListener('mousedown', drag);
 function drag(e){
         var rect = canvas.getBoundingClientRect();
         let x = e.clientX - rect.left;
@@ -365,10 +337,115 @@ function drag(e){
         });
 }
 
+canvas.addEventListener('mousedown', resizeTop);
+function resizeTop(e){
+        var rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        state.boxArr.forEach(function(el){
+                
+                if(x >= el.resize.top.x && x < (el.resize.top.x + el.resize.top.w) && y >= el.resize.top.y && y < el.resize.top.y + el.resize.top.h){
+                        if(keyBoardObj.multiSelect){
+                                activateResizeMulti();        
+                        }
+
+                        resizeObj.xStart = e.clientX;
+                        resizeObj.yStart = e.clientY;
+                        el.activateResize();
+                        resizeObj.element = el;
+                        canvas.addEventListener('mousemove', resizeBoxHeight);
+                } 
+                else if(x >= el.resize.right.x && x < (el.resize.right.x + el.resize.right.w) && y >= el.resize.right.y && y < el.resize.right.y + el.resize.right.h){
+                        if(keyBoardObj.multiSelect){
+                                activateResizeMulti();        
+                        }
+
+                        resizeObj.xStart = e.clientX;
+                        resizeObj.yStart = e.clientY;
+                        el.activateResize();
+                        resizeObj.element = el;
+                        canvas.addEventListener('mousemove', resizeBoxWidth);
+                }
+        })
+}
+
+function activateResizeMulti(){
+        state.boxArr.forEach(function(el){
+                if(el.selected){
+                        el.activateResize();
+                }
+        })
+}
+
+function resizeBoxHeight(e){
+        var rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+        
+        let elWidth = resizeObj.element.x + resizeObj.element.w;
+        let elHeight = resizeObj.element.y + resizeObj.element.h;
+
+        resizeObj.xEnd = e.clientX;
+        resizeObj.yEnd = e.clientY;
+        resizeObj.xDiff = resizeObj.xEnd - resizeObj.xStart;
+        resizeObj.yDiff = resizeObj.yEnd - resizeObj.yStart;
+        resizeObj.xStart = e.clientX;
+        resizeObj.yStart = e.clientY;
+
+        state.boxArr.forEach(function(el, ind){
+                if(el.resizeActive){
+                        el.y += resizeObj.yDiff;
+                        el.resize.top.y += resizeObj.yDiff;
+                        el.h -= resizeObj.yDiff;
+                        if(el.h < 5){
+                                el.h = 5;
+                        }
+                adjustResizeBoxes(el);
+                } 
+        });
+}
+
+function resizeBoxWidth(e){
+        var rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        resizeObj.xEnd = e.clientX;
+        resizeObj.yEnd = e.clientY;
+        resizeObj.xDiff = resizeObj.xEnd - resizeObj.xStart;
+        resizeObj.yDiff = resizeObj.yEnd - resizeObj.yStart;
+        resizeObj.xStart = e.clientX;
+        resizeObj.yStart = e.clientY;
+
+        state.boxArr.forEach(function(el, ind){
+                if(el.resizeActive){
+                        el.resize.right.x += resizeObj.xDiff;
+                        el.w += resizeObj.xDiff;
+                        if(el.w < 5){
+                                el.w = 5;
+                        }
+                adjustResizeBoxes(el)  
+                } 
+        });
+}
+
+function resetResizeActive(){
+        state.boxArr.forEach(function(el){
+                el.deactivateResize();
+        });
+}
+
 canvas.addEventListener('mouseup', function(){
         canvas.style.cursor = '';
         mousePosObj.active = false;
         canvas.removeEventListener('mousemove', moveBox);
+
+        canvas.removeEventListener('mousemove', resizeBoxWidth);
+        canvas.removeEventListener('mousedown', resizeTop);
+        canvas.removeEventListener('mousemove', resizeBoxHeight);
+        resetResizeActive();
+        canvas.addEventListener('mousedown', resizeTop);
 });
 
 function moveBox(e){
@@ -376,28 +453,60 @@ function moveBox(e){
         let x = e.clientX - rect.left;
         let y = e.clientY - rect.top;
         
-                let elWidth = mousePosObj.element.x + mousePosObj.element.w;
-                let elHeight = mousePosObj.element.y + mousePosObj.element.h;
-                
-                mousePosObj.xEnd = e.clientX;
-                mousePosObj.yEnd = e.clientY;
-                mousePosObj.xDiff = mousePosObj.xEnd - mousePosObj.xStart;
-                mousePosObj.yDiff = mousePosObj.yEnd - mousePosObj.yStart;
-                mousePosObj.xStart = e.clientX;
-                mousePosObj.yStart = e.clientY;
+        let elWidth = mousePosObj.element.x + mousePosObj.element.w;
+        let elHeight = mousePosObj.element.y + mousePosObj.element.h;
+        
+        mousePosObj.xEnd = e.clientX;
+        mousePosObj.yEnd = e.clientY;
+        mousePosObj.xDiff = mousePosObj.xEnd - mousePosObj.xStart;
+        mousePosObj.yDiff = mousePosObj.yEnd - mousePosObj.yStart;
+        mousePosObj.xStart = e.clientX;
+        mousePosObj.yStart = e.clientY;
 
-                state.boxArr.forEach(function(el, ind){
-                        if(el.selected){
+        state.boxArr.forEach(function(el, ind){
+                if(el.selected && !el.resizeActive){
                         el.x += mousePosObj.xDiff;
                         el.y += mousePosObj.yDiff;
-
+                        moveResizeBoxes(el, mousePosObj.xDiff, mousePosObj.yDiff);
+                        adjustResizeBoxes(el);
                         keepInBounds(el);
                 }
-               
        });
 };
 
+/* Functions for Resize Boxes */
 
+// Draw Resize Boxes
+function resizeBoxDraw(){
+        state.boxArr.forEach(function(el){
+                ctx.fillStyle = el.resizeStyle;
+                ctx.fillRect(el.resize.top.x, el.resize.top.y, el.resize.top.w, el.resize.top.h)
+                // ctx.fillRect(el.resize.bottom.x, el.resize.bottom.y, el.resize.bottom.w, el.resize.bottom.h)
+                // ctx.fillRect(el.resize.left.x, el.resize.left.y, el.resize.left.w, el.resize.left.h)
+                ctx.fillRect(el.resize.right.x, el.resize.right.y, el.resize.right.w, el.resize.right.h)  
+        })
+        
+}
+
+// Move Resize Boxes
+function moveResizeBoxes(el, xDiff, yDiff){
+        el.resize.top.x += xDiff;
+        el.resize.top.y += yDiff;
+        // el.resize.bottom.x += xDiff;
+        // el.resize.bottom.y += yDiff;
+        el.resize.right.x += xDiff;
+        el.resize.right.y += yDiff;
+        // el.resize.left.x += xDiff;
+        // el.resize.left.y += yDiff;
+}
+
+// Reposition Resize Boxes While Adjusting
+function adjustResizeBoxes(el){
+        el.resize.top.y = el.y - 4;
+        el.resize.top.x = el.x + (el.w / 2);
+        el.resize.right.y = el.y + (el.h / 2) - 4;
+        el.resize.right.x = el.x + el.w - 4;
+}
 
 // Keep box in boundaries of canvas
 function keepInBounds(el){
@@ -428,6 +537,8 @@ function draw(){
         state.boxArr.forEach(function(el){
                 ctx.fillStyle = el.style;
                 ctx.fillRect(el.x, el.y, el.w, el.h);
+                adjustResizeBoxes(el);
+                resizeBoxDraw(el);
         });
         window.requestAnimationFrame(draw);
 }
@@ -439,7 +550,36 @@ class Box{
                 this.w = 100;
                 this.h = 25;
                 this.selected = false;
-                this.style = "rgba(255,0,255, 0.5)";
+                this.style = "rgba(255,0,255, 0.5)",
+                this.resizeActive = false,
+                this.resizeStyle = "rgba(0,200,255,0.8)",
+                this.resize = {
+                        top: {
+                                w: 8,
+                                h: 8,
+                                x: (this.x + this.w) / 2,
+                                y: this.y - 4
+                        },
+                        // bottom: {
+                        //         w: 8,
+                        //         h: 8,
+                        //         x: (this.x + this.w) / 2,
+                        //         y: (this.y + this.h) - 4 / 2
+                        // },
+                        // left: {
+                        //         w: 8,
+                        //         h: 8,
+                        //         x: this.x - 4,
+                        //         y: (this.y + this.h) / 2
+                        // },
+                        right: {
+                                w: 8,
+                                h: 8,
+                                x: this.x + this.w - 4,
+                                y: ((this.y + this.h) / 2) - 4
+                        }
+
+                }
         }
 
         select(){
@@ -450,6 +590,16 @@ class Box{
         deSelect(){
                 this.selected = false;
                 this.style = "rgba(255,0,255, 0.5)";
+        }
+
+        activateResize(){
+                this.resizeActive =  true;
+                this.resizeStyle = "rgba(0,255,150,0.8)"
+        }
+
+        deactivateResize(){
+                this.resizeActive = false;
+                this.resizeStyle = "rgba(0,200,255,0.8)"
         }
 }
 
